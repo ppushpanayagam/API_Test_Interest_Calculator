ef0776a3f89b6644d7823097aab7603880250fa6
Pushparaj Pushpanayagam <ppushpanayagam@gmail.com>
2024-05-11 16:23:02 +0100

    added custom runner using index.ts and shell script. Also added an assertion point

--- e2e/src/features/GET.feature
+++ e2e/src/features/GET.feature
@@ -4,3 +4,4 @@ Feature: As an API I can retrive all the details
   Scenario: As an API I can GET all the details
     Given I get the "interestCalculator"
     And the response was successful
+    Then the response status code is 200
--- e2e/src/step-definitions/assertions/response-steps.ts
+++ e2e/src/step-definitions/assertions/response-steps.ts
@@ -21,3 +21,17 @@ Then(
 
     }
 )
+
+Then(
+    /^the response status code is (\d*)$/,
+    async function(this: ScenarioWorld, statusCode: string) {
+        const {
+            globalAPIResponseVariables
+        } = this
+
+        const response = globalAPIResponseVariables.response
+
+        expect(response.status()).toBe(Number(statusCode))
+
+    }
+)
