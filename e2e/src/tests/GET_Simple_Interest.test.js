315aed0f4a04402c491831d26ab112427d01c686
Pushparaj Pushpanayagam <ppushpanayagam@gmail.com>
2024-05-12 05:21:40 +0100

    removed bare bone initial js test and report

--- e2e/src/tests/GET_Simple_Interest.test.js
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('retrieve user posts', async ({ request}) => {
-    const response = await request.get('interest/calculate?amount=1000&interestRate=5&duration=3&accrualType=SIMPLE')
-    expect(response.ok()).toBeTruthy()
-    expect(response.status()).toBe(200)
-})
